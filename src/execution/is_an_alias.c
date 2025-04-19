75ced8774668ccdba4b4638018fc202d88de5579
rayan <rayan@epitech.eu>
2025-04-20 01:55:43 +0200

    feat(app): add alias like the true tcsh

---
+++ src/execution/is_an_alias.c
@@ -0,0 +1,29 @@
+/*
+** EPITECH PROJECT, 2025
+** minishell
+** File description:
+** is an alias
+*/
+
+#include "shell.h"
+#include <stdio.h>
+
+alias_t **get_list_alias(void)
+{
+    static alias_t *list = NULL;
+
+    return &list;
+}
+
+char *is_an_alias(char *command)
+{
+    alias_t **list = get_list_alias();
+    alias_t *current = (*list);
+
+    while (current) {
+        if (!strcmp(current->shortcut, command))
+            return current->command;
+        current = current->next;
+    }
+    return command;
+}
