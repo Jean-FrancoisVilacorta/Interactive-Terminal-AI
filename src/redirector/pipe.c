56a16c33f0e92765a15a6a98a8021aa138ffbf3f
rayan <rayan.ouerdane@epitech.eu>
2025-05-03 02:18:11 +0200

    fix(app): fix big input(i hope)

--- src/redirector/pipe.c
+++ src/redirector/pipe.c
@@ -33,21 +33,22 @@ static void execute_com_end(bintree_t *tree, int *fd_pipe,
 int my_pipe(bintree_t *node, char ***env, int *status)
 {
     int fd_pipe[2];
-    pid_t pid = -1;
+    pid_t pid1 = -1;
+    pid_t pid2 = -1;
 
     if (!node->left || !node->right)
         return FAIL;
     if (pipe(fd_pipe) == FAIL)
         return FAIL;
-    pid = fork();
-    if (pid == 0)
+    pid1 = fork();
+    if (pid1 == 0)
         execute_com(node->left, fd_pipe, status, env);
-    waitpid(pid, status, 0);
-    pid = fork();
-    if (pid == 0)
+    pid2 = fork();
+    if (pid2 == 0)
         execute_com_end(node->right, fd_pipe, status, env);
     close(fd_pipe[READ]);
     close(fd_pipe[WRITE]);
-    waitpid(pid, status, 0);
+    waitpid(pid1, status, 0);
+    waitpid(pid2, status, 0);
     return *status;
 }
